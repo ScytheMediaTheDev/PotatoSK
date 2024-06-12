7b6389f0fcd91dc6113e87eb4879912cfc91a8a6
MCdragonmasters <natedragonmaster@gmail.com>
2024-06-12 15:49:22 -0400

    fr

--- src/main/java/com/mcdragonmasters/PotatoSK/skript/effects/EffCommandWithPermission.java
+++ src/main/java/com/mcdragonmasters/PotatoSK/skript/effects/EffCommandWithPermission.java
@@ -1,7 +1,13 @@
 package com.mcdragonmasters.PotatoSK.skript.effects;
 
+import ch.njol.skript.lang.VariableString;
+import ch.njol.skript.util.StringMode;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.permissions.Permissible;
+import org.bukkit.permissions.Permission;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Effect;
@@ -12,13 +18,13 @@ import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.util.Kleenean;
+import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.Server;
-import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
-import org.bukkit.permissions.Permissible;
 
-import javax.security.auth.kerberos.KerberosTicket;
+import java.util.HashMap;
+
+import static com.mcdragonmasters.PotatoSK.PotatoSK.instance;
 
 import static org.bukkit.Bukkit.getServer;
 
@@ -30,35 +36,51 @@ import static org.bukkit.Bukkit.getServer;
 
 public class EffCommandWithPermission extends Effect{
 
+    private static final HashMap<Permissible, String> map = new HashMap<>();
+
     static {
         Skript.registerEffect(EffCommandWithPermission.class,
-                "execute %player% command %string% with permission %string%"
+                "execute %player% command %string% (with|using) %bukkitpermission%"
         );
     }
     private Expression<String> command;
-    private Expression<String> perm;
-    private Expression<Player> effplayer;
+    private Expression<Permission> perm;
+    private Expression<Player> senders;
 
     @Override
     @SuppressWarnings("unchecked")
     public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull ParseResult parseResult) {
-        effplayer = (Expression<Player>) exprs[1];
-        command = (Expression<String>) exprs[2];
-        perm = (Expression<String>) exprs[3];
+        senders = (Expression<Player>) exprs[0];
+        command = (Expression<String>) exprs[1];
+        perm = (Expression<Permission>) exprs[2];
+        command = VariableString.setStringMode(command, StringMode.COMMAND);
         return true;
     }
     @Override
-    protected void execute(Event e) {
-        Plugin skript = getServer().getPluginManager().getPlugin("Skript");
-        Player player = (Player) effplayer;
-        player.addAttachment(skript, String.valueOf(perm), true, 1);
-        System.out.println("test effect used");
-
+    protected void execute(@NotNull Event e) {
+        if (senders != null && perm != null) {
+            Permission perm = this.perm.getSingle(e);
+            String command = this.command.getSingle(e);
+            if (senders == null || command == null){
+                return;
+            }
+            for (Player p : senders.getArray(e)) {
+                map.put(p, command);
+                PermissionAttachment attachment = new PermissionAttachment(instance, p);
+                assert perm != null;
+                attachment.setPermission(perm, true);
+                p.performCommand(command);
+                attachment.remove();
+                map.remove(p, command);
+            }
+        }
     }
+
+
 
     @Override
     public @NotNull String toString(Event e, boolean debug) {
-        return "execute " + effplayer + " command " + command + "with permission " + perm; // In case of using a variable, use your variable as "yourvariable.toString(e, debug)"
+        return "execute " + senders + " command " + command + "with permission " + perm; // In case of using a variable, use your variable as "yourvariable.toString(e, debug)"
     }
 
 }
