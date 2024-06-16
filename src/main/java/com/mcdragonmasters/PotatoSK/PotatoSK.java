bab3aa993db3a8f8345b3a1cea6fcf2fdbdf1cf7
MCdragonmasters <natedragonmaster@gmail.com>
2024-06-16 15:33:48 -0400

    fr

--- src/main/java/com/mcdragonmasters/PotatoSK/PotatoSK.java
+++ src/main/java/com/mcdragonmasters/PotatoSK/PotatoSK.java
@@ -1,18 +1,17 @@
 package com.mcdragonmasters.PotatoSK;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import com.mcdragonmasters.PotatoSK.utils.PackageLoader;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
-//import com.mcdragonmasters.PotatoSK.utils.PackageLoader;
 import com.olyno.skriptmigrate.SkriptMigrate;
-
+import ch.njol.skript.bstats.bukkit.Metrics;
+import ch.njol.skript.bstats.charts.SimplePie;
 import ch.njol.skript.Skript;
 import ch.njol.skript.SkriptAddon;
 
@@ -21,6 +20,9 @@ public class PotatoSK extends JavaPlugin {
 
     public static PotatoSK instance;
     public static FileConfiguration config;
+    private static Metrics metrics;
+    private static String prefix = net.md_5.bungee.api.ChatColor.of("#40ff00")
+            + "[PotatoSK] " + ChatColor.RESET;
     SkriptAddon addon;
 
 	public void onEnable() {
@@ -34,24 +36,13 @@ public class PotatoSK extends JavaPlugin {
         }
 
         // Register Metrics
-        Metrics metrics = new Metrics(this);
-        metrics.addCustomChart(new Metrics.DrilldownPie("java_version", () -> {
-            Map<String, Map<String, Integer>> map = new HashMap<>();
-            String javaVersion = System.getProperty("java.version");
-            Map<String, Integer> entry = new HashMap<>();
-            entry.put(javaVersion, 1);
-            if (javaVersion.startsWith("1.7")) {
-                map.put("Java 1.7", entry);
-            } else if (javaVersion.startsWith("1.8")) {
-                map.put("Java 1.8", entry);
-            } else if (javaVersion.startsWith("1.9")) {
-                map.put("Java 1.9", entry);
-            } else {
-                map.put("Other", entry);
-            }
-            return map;
-        }));
+        Metrics metrics = new Metrics(this, 22275);
 
+        metrics.addCustomChart(new SimplePie("skript_version", () -> Skript.getVersion().toString()));
+        Bukkit.getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "PotatoSK has been enabled!");
+//        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+//            player.sendMessage(prefix + "fr");
+//        }
          //Register events
         PluginManager pm = getServer().getPluginManager();
         //pm.registerEvents(new JoinLeave(), this);
